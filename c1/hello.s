00be6d770e2ba7f646e3f75a6d473589857a24ce
zhengwei <zhengwei@zhengweideMacBook-Pro.local>
2015-11-16 21:21:27 +0800

    初次上传代码

---
+++ c1/hello.s
@@ -0,0 +1,34 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 11
+	.globl	_main
+	.align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	pushq	%rbp
+Ltmp0:
+	.cfi_def_cfa_offset 16
+Ltmp1:
+	.cfi_offset %rbp, -16
+	movq	%rsp, %rbp
+Ltmp2:
+	.cfi_def_cfa_register %rbp
+	subq	$16, %rsp
+	leaq	L_.str(%rip), %rdi
+	movl	$0, -4(%rbp)
+	movb	$0, %al
+	callq	_printf
+	xorl	%ecx, %ecx
+	movl	%eax, -8(%rbp)          ## 4-byte Spill
+	movl	%ecx, %eax
+	addq	$16, %rsp
+	popq	%rbp
+	retq
+	.cfi_endproc
+
+	.section	__TEXT,__cstring,cstring_literals
+L_.str:                                 ## @.str
+	.asciz	"\350\277\231\346\230\257\346\210\221\347\232\204\347\254\254\344\270\200\344\270\252C\347\250\213\345\272\217\n"
+
+
+.subsections_via_symbols
---
+++ c3/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+//主函数
+int main(){
+    //在此处插入代码
+    printf("这是我的第一个C程序\n");
+    return 0;	
+}
